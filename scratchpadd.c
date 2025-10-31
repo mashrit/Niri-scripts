c7b805913a0b8de232bd6354615a37dc20be42d7
mashrit <patoliyamashrit@gmail.com>
2025-10-31 21:09:02 +0530

    improved scractchpadd.c

--- scratchpadd.c
+++ scratchpadd.c
@@ -24,102 +24,149 @@ static char* get_cmd_output(const char *cmd)
 	return buffer;
 }
 
+void get_work_idx(int currWorkId, int *currWorkIdx, int *lastWorkIdx)
+{
+	// --- Get workspaces ---
+	char *workspaces_json = get_cmd_output("niri msg -j workspaces");
+	if (!workspaces_json)
+	{
+		*currWorkIdx = -1;
+		*lastWorkIdx = -1;
+	}
+
+	json_error_t error;
+	json_t *workspaces = json_loads(workspaces_json, 0, &error);
+	if (!workspaces)
+	{
+		*currWorkIdx = -1;
+		*lastWorkIdx = -1;
+	}
+
+	json_t *ws;
+	size_t i;
+	json_array_foreach(workspaces, i, ws)
+	{
+		json_t *idx = json_object_get(ws, "idx");
+		json_t *id = json_object_get(ws, "id");
+		json_t *activeWin = json_object_get(ws, "active_window_id");
+
+		if (!json_integer_value(activeWin))
+			*lastWorkIdx = json_integer_value(idx);
+
+		if (json_integer_value(id) == currWorkId)
+			*currWorkIdx = json_integer_value(idx);
+	}
+	json_decref(workspaces);
+}
+
+int get_curr_work_id(json_t *event)
+{
+	const char *type;
+	json_t *data;
+
+	void *iter = json_object_iter(event);
+	if (!iter)
+	    return -1;
+	
+	type = json_object_iter_key(iter);
+	data = json_object_iter_value(iter);
+	
+	if (strcmp(type, "WorkspaceActivated") == 0) 
+	{
+	    json_t *id = json_object_get(data, "id");
+	    if (json_is_integer(id)) 
+			return json_integer_value(id);
+	}
+
+	return -1;
+}
+
 int main(void)
 {
 	daemon(0, 0);
 
-	while (1)
+	FILE *pipe = popen("niri msg -j event-stream","r");
+	if (!pipe)
 	{
-		if (access("/tmp/scratchpad-closed", F_OK) == -1)
-		{
-			system("scratchpad clear");
-			usleep(CHECK_INTERVAL_US);
-			continue;
-		}
-
-		// --- Get all windows ---
-		char *windows_json = get_cmd_output("niri msg -j windows");
-		if (!windows_json)
-		{
-			usleep(CHECK_INTERVAL_US);
-			continue;
-		}
-
-		json_error_t error;
-		json_t *windows = json_loads(windows_json, 0, &error);
-		if (!windows)
-		{
-			usleep(CHECK_INTERVAL_US);
-			continue;
-		}
-
-		// Find window with title "cm"
-		json_t *window;
-		int window_id = -1;
-		size_t i;
-		json_array_foreach(windows, i, window)
-		{
-		    json_t *title = json_object_get(window, "title");
-
-			if(!title)
-				break;
-
-		    if (strcmp(json_string_value(title), "cm") == 0)
-			{
-		        json_t *id = json_object_get(window, "id");
-		        if (id)
-					window_id = json_integer_value(id);
-
-		        break;
-		    }
-		}
-		json_decref(windows);
-
-		if (window_id == -1)
-		{
-			system("scratchpad clear");
-			usleep(CHECK_INTERVAL_US);
-			continue;
-		}
-
-		// --- Get workspaces ---
-		char *workspaces_json = get_cmd_output("niri msg -j workspaces");
-		if (!workspaces_json)
-		{
-			usleep(CHECK_INTERVAL_US);
-			continue;
-		}
-
-		json_t *workspaces = json_loads(workspaces_json, 0, &error);
-		if (!workspaces)
-		{
-			usleep(CHECK_INTERVAL_US);
-			continue;
-		}
-
-		int window_workspace_idx = -1;
-		int current_workspace_idx = -1;
-		json_t *ws;
-		json_array_foreach(workspaces, i, ws)
-		{
-			json_t *active_win = json_object_get(ws, "active_window_id");
-			json_t *idx = json_object_get(ws, "idx");
-			json_t *focused = json_object_get(ws, "is_focused");
-
-			if (active_win && json_integer_value(active_win) == window_id)
-		    	window_workspace_idx = json_integer_value(idx);
-
-			if (focused && json_is_true(focused))
-		    	current_workspace_idx = json_integer_value(idx);
-		}
-		json_decref(workspaces);
-
-		// --- Compare and act ---
-		if (window_workspace_idx == current_workspace_idx && window_workspace_idx >= 0)
-			system("niri msg action move-window-to-workspace-down --focus false");
-
-		usleep(CHECK_INTERVAL_US);
+	    printf("Failed to open niri event stream");
+	    return 1;
 	}
+	
+
+	json_error_t error;
+	int cmId = -1;
+
+	char *windows_json = get_cmd_output("niri msg -j windows");
+	if (!windows_json)
+		return 1;
+
+	json_t *windows = json_loads(windows_json, 0, &error);
+	if (!windows)
+	{
+		fprintf(stderr, "JSON parse error: %s\n", error.text);
+		return 1;
+	}
+
+	json_t *window;
+	size_t i = 0;
+	json_array_foreach(windows, i, window)
+	{
+		json_t *title = json_object_get(window,"title");
+		json_t *id = json_object_get(window, "id");
+
+		if(title && strcmp(json_string_value(title), "cm") == 0)
+		{
+			cmId = json_integer_value(id);
+			break;
+		}
+	}
+	json_decref(windows);
+
+
+	char buffer[8192];
+	while(fgets(buffer, sizeof(buffer), pipe))
+	{
+		if(access("/tmp/scratchpad-closed", F_OK) == -1)
+		{
+			system("scratchpad clear");
+			continue;
+		}
+
+		char *pgrepAns = get_cmd_output("pgrep cm");
+		if (!pgrepAns)
+		{
+			system("scratchpad clear");
+			continue;
+		}
+
+		json_t *event = json_loads(buffer, 0, &error);
+		if (!event)
+		{
+			fprintf(stderr, "JSON event parse error: %s\n", error.text);
+			continue;
+		}
+
+		int currWorkId = -1;
+		currWorkId = get_curr_work_id(event);
+		if (currWorkId == -1)
+			continue;
+
+		int lastWorkIdx = -1;
+		int currWorkIdx = -1;
+		get_work_idx(currWorkId, &currWorkIdx, &lastWorkIdx);
+		if (lastWorkIdx == -1 || currWorkIdx == -1)
+			continue;	
+
+		if (currWorkIdx >= lastWorkIdx - 1)
+		{
+			char command[100];
+			sprintf(command, "niri msg action move-window-to-workspace --window-id %d --focus false %d", cmId, lastWorkIdx);
+			system(command);
+		}
+		
+		json_decref(event);
+	}
+	
 
 	return 0;
 }
